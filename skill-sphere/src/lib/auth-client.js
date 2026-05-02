27df0b577cc7876f1d58c4ae51f20035c5672c15
Munaim Khan <khanmail2599@gmail.com>
2026-05-02 22:15:04 +0600

    fix:add live url to the auth-client.js

--- skill-sphere/src/lib/auth-client.js
+++ skill-sphere/src/lib/auth-client.js
@@ -3,7 +3,7 @@ import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: "https://skill-sphere-munaimpro.vercel.app"
 })
 
 export const { signIn, signUp, useSession } = createAuthClient()
